b2508be81f5c7005d1ad29c5b51a69529862aa6b
Kristen De La Cerda <kndelacerda@gmail.com>
2017-06-14 18:42:38 -0500

    Removed app.get

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
     });
 
     // If no matching route is found default to home
-    app.use(function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));
-    });
+    // app.get(function(req, res) {
+    //     res.sendFile(path.join(__dirname, "/../public/home.html"));
+    // });
 };
