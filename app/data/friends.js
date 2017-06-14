b9aac4e121993cecc810b6b4f752a9979a3f427e
Kristen De La Cerda <kndelacerda@gmail.com>
2017-06-14 18:34:18 -0500

    Reformatted

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [{
-    name: "Kristen",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAO6AAAAJGJkNTM5OTMwLWFmMDUtNDczNC1hMTdkLTliYWE0YWZhNTQxZQ.jpg",
-    scores: [
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
         5,
         1,
         4,
@@ -14,9 +14,24 @@ var friends = [{
         1
     ]
 }, {
-    name: "Alex Morgan",
-    photo: "http://www.famousbirthdays.com/headshots/alex-morgan-3.jpg",
-    scores: [
+    "name": "Kristen",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAO6AAAAJGJkNTM5OTMwLWFmMDUtNDczNC1hMTdkLTliYWE0YWZhNTQxZQ.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "Alex Morgan",
+    "photo": "http://www.famousbirthdays.com/headshots/alex-morgan-3.jpg",
+    "scores": [
         4,
         1,
         4,
@@ -29,9 +44,9 @@ var friends = [{
         1
     ]
 }, {
-    name: "Phil Dunphy",
-    photo: "https://cdn.pastemagazine.com/www/blogs/lists/2012/07/20/phil-dunphy.jpg",
-    scores: [
+    "name": "Phil Dunphy",
+    "photo": "https://cdn.pastemagazine.com/www/blogs/lists/2012/07/20/phil-dunphy.jpg",
+    "scores": [
         3,
         3,
         3,
@@ -44,9 +59,9 @@ var friends = [{
         3
     ]
 }, {
-    name: "Natalie Coughlin",
-    photo: "http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/natalie-olympics.jpg",
-    scores: [
+    "name": "Natalie Coughlin",
+    "photo": "http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/natalie-olympics.jpg",
+    "scores": [
         5,
         1,
         5,
